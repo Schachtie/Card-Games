eb053bdc18023c3f6ac83d0a531ce66090cd7f5c
Trenton Schacht <trentonschacht@gmail.com>
2024-03-13 15:31:44 -0400

    Added functions in main file to clean up main. Allows user to enter a username.

--- Card Games/CardGames.cpp
+++ Card Games/CardGames.cpp
@@ -5,28 +5,45 @@
 *			Classes written to be extensible so new card games could be added.
 */
 
-//Header Files
+// Header Files
 #include <iostream>
 
 #include "User.h"
 #include "Game_5CardDraw.h"
 #include "Game_VideoPoker.h"
 
-//Using Statements
 using namespace std;
 
 
-//Main Function
+
+// Function Prototypes
+void mainMenu(User* pMainUser);
+void welcomeUser(User* pMainUser);
+
+
+
+// Main Function
 int main()
 {
-	User defaultUser;
-	User trentUser("Trent");
-
-	cout << defaultUser.getUserName() << " has " << defaultUser.getCredits() << " credits." << endl;
-	cout << trentUser.getUserName() << " has " << trentUser.getCredits() << " credits." << endl;
-
+	User mainUser;
+	welcomeUser(&mainUser);
+	mainMenu(&mainUser);
+} //end of "main"
 
 
+
+// Function Definitions
+
+/*	mainMenu
+* 
+*	@note:	Displays main menu of games (and exit) for user to choose from.
+*			Can add new games by adjusting the menu and adding a switch case.
+* 
+*	@param: Pointer to user created in main.
+* 
+*	@return: void
+*/
+void mainMenu(User* pMainUser) {
 	while (true) {
 		//Main Menu
 		cout << "\n-=-=-=-=-=-=-=-=-=-=-=-=-= Card Games =-=-=-=-=-=-=-=-=-=-=-=-=-" << endl;
@@ -43,26 +60,26 @@ int main()
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			if (cin.good()) {
 				switch (iInput) {
-					case 1: { //FCD
-						Game_5CardDraw fiveCardDraw(&trentUser);
-						fiveCardDraw.run();
-						break;
-					}
-					case 2: { //VP
-						Game_VideoPoker videoPoker(&trentUser);
-						videoPoker.run();
-						break;
-					}
-					case 0: { //Close
-						cout << "\nThank you for playing!" << endl;
-						cout << "\n\n" << trentUser.getUserName() << " has " << trentUser.getCredits() << " credits." << endl;
-						return 0;
-					}
-					default: { //Input error
-						iInput = -1;
-						cout << "\tPlease enter a valid option." << endl;
-						break;
-					}
+				case 1: { //FCD
+					Game_5CardDraw fiveCardDraw(pMainUser);
+					fiveCardDraw.run();
+					break;
+				}
+				case 2: { //VP
+					Game_VideoPoker videoPoker(pMainUser);
+					videoPoker.run();
+					break;
+				}
+				case 0: { //Close
+					cout << "\nThank you for playing!" << endl;
+					cout << "\n\n" << pMainUser->getUserName() << " ended with " << pMainUser->getCredits() << " credits." << endl;
+					return;
+				}
+				default: { //Input error
+					iInput = -1;
+					cout << "\tPlease enter a valid option." << endl;
+					break;
+				}
 				}
 			}
 			else {
@@ -73,4 +90,44 @@ int main()
 			}
 		}
 	}
-} //end of "main"
+} //end of "mainMenu"
+
+
+/*	welcomeUser
+* 
+*	@note:	Welcomes user to program and allows them to enter a username.
+*			Handles user input.
+* 
+*	@param: Pointer to user created in main.
+* 
+*	@return: void
+*/
+void welcomeUser(User* pMainUser) {
+	cout << "\\____________________ Welcome to Card Games! ____________________/\n" << endl;
+	
+	while (true) {
+		cout << "Please enter a Username: ";
+		string userName;
+		getline(cin, userName);
+
+		while (true) {
+			cout << "Welcome, " << userName << endl;
+			cout << "Is this Username correct? (Yes/No): ";
+			string input;
+			getline(cin, input);
+
+			if (input == "yes" || input == "Yes" || input == "YES") {
+				pMainUser->setUserName(userName);
+				cout << '\n' << pMainUser->getUserName() << " is given " << pMainUser->getCredits() << " credits to start." << endl;
+				cout << "Have fun!\n" << endl;
+				return;
+			}
+			else if (input == "no" || input == "No" || input == "NO") {
+				break;
+			}
+
+			cout << "Please enter a valid option. ";
+			cin.clear();
+		}
+	}
+} //end of "welcomeUser"
